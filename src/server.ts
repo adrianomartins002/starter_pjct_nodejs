a85f21d7b0155bcd59b2d15c826657087feaeee1
Adriano Martins <adrianomartins002@gmail.com>
2019-12-11 21:24:18 -0300

    gitflow-feature-stash: f_midproject
    feat(mongo, middleware, estrutura): adicionar estrutura do projeto e bibliotecas mongoose, cors, types para melhor estrutura.

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,10 @@
+import express from 'express'
+import { applyMiddleware } from './utils'
+import middleware from './middleware'
 import app from './app'
 
-app.listen(3333)
+const router = express()
+applyMiddleware(middleware, router.use(app))
+
+const { PORT = 8001 } = process.env
+router.listen(PORT, () => console.log(`Server is running http://localhost:${PORT}...`))

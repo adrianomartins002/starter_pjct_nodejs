53b857bd17fc89874297d4dba1e3b64b61053f19
Adriano <adrianomartins002@gmail.com>
2019-12-17 13:54:20 -0300

    gitflow-feature-stash: autenticacao
    feat(autenticacao): alterar arquivos e adicionar etapa de autenticacao

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,54 @@
+
+import { Request, Response } from 'express'
+import { User } from '../schemas/User'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+const authConfig = require('../config/auth')
+
+class UserController {
+    public async index(req: Request, res: Response): Promise<Response> {
+        const users = await User.find()
+
+        return res.json(users)
+    }
+
+    public async register(req: Request, res: Response): Promise<Response> {
+        console.log("teste:", req.body)
+        const { email } = req.body;
+        try {
+            if (await User.findOne({ email }))
+                return res.status(400).send({ error: 'Usuario já existe' });
+
+            const user = await User.create(req.body);
+
+            user.password = undefined;
+
+            return res.send({ user });
+        } catch (e) {
+            return res.status(400).send({ error: 'Falha ao registrar' })
+        }
+    }
+
+    public async authenticate(req: Request, res: Response): Promise<Response> {
+        const { email, password } = req.body
+
+        const user = await User.findOne({ email }).select('+password')
+
+        if (!user) {
+            return res.status(400).send({ error: 'Usuario nao encontrado' })
+        }
+
+        if (!await bcrypt.compare(password, user.password)) {
+            return res.status(400).send({ error: 'Senha invalida' })
+        }
+
+        const token = jwt.sign({ id: user.id }, authConfig.secret, {
+            expiresIn:86400
+        })
+
+        res.send({ user, token })
+    }
+
+}
+
+export default new UserController()
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express'
 import DadoController from './controllers/DadoController'
+import UserController from './controllers/UserController'
 
 const routes = Router()
 
 routes.get('/dado', DadoController.find)
 routes.post('/dado', DadoController.create)
+
+routes.post('/user', UserController.register)
+routes.get('/user', UserController.index)
 
 export default routes

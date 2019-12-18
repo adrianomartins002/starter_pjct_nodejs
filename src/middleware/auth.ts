880952f85a69de96648bff0610972597bf1236ee
Adriano Martins <adrianomartins002@gmail.com>
2019-12-18 20:25:35 -0300

    gitflow-hotfix-stash: correcoes

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,14 +1,14 @@
 import jwt from 'jsonwebtoken'
 import { Request, Response } from 'express'
-const authConfig = require('../config/auth')
+import authConfig from '../config/auth.json'
 
 export class Auth {
-  public static auth (req: Request, res: Response, next) {
+  public static auth (req: Request, res: Response, next):Response {
     const authHeader = req.headers.authorization
 
     if (!authHeader) { return res.status(401).send({ error: 'Sem token!' }) }
 
-    const parts = authHeader.split(' ')
+    const parts:Array<string> = authHeader.split(' ')
 
     if (!parts.length == 2) { return res.status(401).send({ error: 'Token error!' }) }
 
@@ -16,7 +16,7 @@ export class Auth {
 
     if (!/^Bearer$/i.test(scheme)) { return res.status(401).send({ error: 'Token mal formatado' }) }
 
-    jwt.verify(token, authConfig.secret, (err, decoded) => {
+    jwt.verify(token, authConfig.secret, (err, decoded:any) => {
       if (err) return res.status(401).send({ error: 'Token invalido' })
 
       req.userId = decoded.id
